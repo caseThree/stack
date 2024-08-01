ffdd9b66be82b22e5efdcf961ead2c05127287d8
Candy_Tewari <sansshobhit@gmail.com>
2024-08-01 15:44:20 +0530

    Completed Implementation

--- stack.go
+++ stack.go
@@ -1,11 +1,76 @@
 package stack
 
-type intStack struct {}
+import (
+	"errors"
+	"sync"
+)
+
+type intStack struct {
+	list []int
+	mux sync.Mutex
+}
 
 type IStack interface {
+	Empty() bool
+	Add(int)
+	Top() (int, error)
+	Pop() (int, error)
+	Size() int
+	Reset()
+}
 
+func (st *intStack) Empty() bool {
+	st.mux.Lock()
+	defer st.mux.Unlock()
+	return len(st.list) == 0
+}
+
+func (st *intStack) intEmpty() bool {
+	return len(st.list) == 0
+}
+
+
+func (st *intStack) Add(val int) {
+	st.mux.Lock()
+	defer st.mux.Unlock()
+	st.list = append(st.list, val)
+}
+
+func (st *intStack) Top() (int, error) {
+	st.mux.Lock()
+	defer st.mux.Unlock()
+	if st.intEmpty() {
+		return 0, errors.New("stack is empty. cannot top")
+	}
+	return st.list[len(st.list)-1], nil
+}
+
+func (st *intStack) Pop() (int, error) {
+	st.mux.Lock()
+	defer st.mux.Unlock()
+	if st.intEmpty() {
+		return 0, errors.New("stack is empty. cannot pop")
+	}
+	val := st.list[len(st.list)-1]
+	st.list = st.list[:len(st.list)-1]
+	return val, nil
+}
+
+func (st *intStack) Size() int {
+	st.mux.Lock()
+	defer st.mux.Unlock()
+	return len(st.list)
+}
+
+func (st *intStack) Reset() {
+	st.mux.Lock()
+	defer st.mux.Unlock()
+	st.list = make([]int, 0)
 }
 
 func Create() IStack {
-	return &intStack{}
+	return &intStack{
+		list: make([]int, 0),
+		mux: sync.Mutex{},
+	}
 }
--- stack_test.go
+++ stack_test.go
@@ -5,5 +5,118 @@ import (
 )
 
 func TestCreate(test *testing.T) {
-	_ = Create()
+	var _ IStack = Create()
+}
+
+func TestEmpty(test *testing.T) {
+	st := Create()
+	if st.Empty() == false {
+		test.Errorf("Size of stack is not empty")
+	}
+}
+
+func TestAdd(test *testing.T) {
+	st := Create()
+	st.Add(5)
+}
+
+func TestNonEmpty(test *testing.T) {
+	st := Create()
+	st.Add(5)
+	if st.Empty() == true {
+		test.Errorf("Size of stack is non empty")
+	}
+}
+
+func TestTop(test *testing.T) {
+	st := Create()
+	_, ok := st.Top()
+	if ok == nil {
+		test.Errorf("Empty stack top")
+	}
+}
+
+func TestNonEmptyTop(test *testing.T) {
+	st := Create()
+	st.Add(4)
+	st.Add(2)
+	val, ok := st.Top()
+	if ok != nil {
+		test.Errorf("Non empty stack error %v", ok.Error())
+	}
+	if val != 2 {
+		test.Errorf("Top value should be 2")
+	}
+}
+
+func TestPop(test *testing.T) {
+	st := Create()
+	_, ok := st.Pop()
+	if ok == nil {
+		test.Errorf("Empty stack cannot pop")
+	}
+}
+
+func TestSize(test *testing.T) {
+	st := Create()
+	st.Add(4)
+	st.Add(6)
+	st.Add(896)
+	if st.Size() != 3 {
+		test.Errorf("Stack size should be 3")
+	}
+}
+
+func TestPopDecreaseSize(test *testing.T) {
+	st := Create()
+	for i:=1; i<=10; i++ {
+		st.Add(i)
+	}
+	if st.Size() != 10 {
+		test.Errorf("Stack size should be 10")
+	}
+	st.Pop()
+	if st.Size() != 9 {
+		test.Errorf("Stack size should be 9")
+	}
+	st.Pop()
+	st.Pop()
+	st.Pop()
+	st.Pop()
+	if st.Size() != 5 {
+		test.Errorf("Stack size should be 5")
+	}
+}
+
+func TestNonEmptyPop(test *testing.T) {
+	st := Create()
+	st.Add(2)
+	st.Add(-65)
+	val, ok := st.Pop()
+	if ok != nil {
+		test.Errorf("Non empty stack pop error")
+	}
+	if val != -65 {
+		test.Errorf("Top value should be -65")
+	}
+	if val, ok := st.Top(); ok != nil || val != 2 {
+		test.Errorf("Stack top should be 2")
+	}
+	if st.Size() != 1 {
+		test.Errorf("Stack size should br 1")
+	}
+}
+
+func TestReset(test *testing.T) {
+	st := Create()
+	st.Add(5)
+	st.Add(6)
+	st.Add(8)
+	if st.Empty() == true {
+		test.Errorf("Stack should not be empty")
+	}
+	st.Reset()
+	if st.Empty() == false {
+		test.Errorf("Stack should be empty")
+	}
 }
